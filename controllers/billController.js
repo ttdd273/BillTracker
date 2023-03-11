d9e2be0a54035dc864f68eda54c02f8911ef1b61
TTDD <ttdd2001@gmail.com>
2023-03-11 18:00:13 -0500

    Getting data for bills

--- controllers/billController.js
+++ controllers/billController.js
@@ -1,9 +1,38 @@
 const express = require("express");
 
-exports.index = (req, res) => {
-  res.render("bill_list", {
-    title: "Bills",
-  });
+// for bills, we need to import the corresponding models
+const Bill = require("../models/bill");
+// because bills have users, we will also need to import that
+const User = require("../models/user");
+
+// we need the async module to get the database data quickly
+const async = require("async");
+
+// Here's a good example of how to use the find function
+/**
+ * const Athlete = mongoose.model("Athlete", yourSchema);
+ *
+ * // find all athletes who play tennis, selecting the 'name' and 'age' fields
+ * Athlete.find({ sport: "Tennis" }, "name age", (err, athletes) => {
+ *   if (err) return handleError(err);
+ *   // 'athletes' contains the list of athletes that match the criteria.
+ * });
+ */
+
+// we will render all the bills we currently have
+exports.index = function (req, res, next) {
+  Bill.find({}, "name amount userId")
+    .exec()
+    .then((list_bills) => {
+      // successful, so render the list
+      res.render("bill_list", {
+        title: "Bills",
+        bill_list: list_bills,
+      });
+    })
+    .catch((err) => {
+      return next(err);
+    });
 };
 
 exports.bill_create_get = (req, res) => {
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,48 @@
 var express = require("express");
 var router = express.Router();
-
 // const bill_controller = require("../controllers/billController");
 // const payment_controller = require("../controllers/paymentController");
 
+const Bill = require("../models/bill");
+const Payment = require("../models/payment");
+
+const async = require("async");
+
+// we want to get all the basic information on the home page
+// using an async parallel call
+
+/* Unforunately, mongoose 7 doesn't accept callbacks anymore*/
 /* GET home page. */
+// router.get("/", function (req, res, next) {
+//   // we are defining new functions in this async parallel function
+//   async.parallel(
+//     {
+//       bill_count(callback) {
+//         Bill.countDocuments({}, callback); // Pass an empty object as match condition to find all documents in this collection
+//       },
+//       payment_count(callback) {
+//         Payment.countDocuments({}, callback);
+//       },
+//     },
+//     (err, results) => {
+//       res.render("index", { title: "Bill Tracker", error: err, data: results });
+//     }
+//   );
+// });
+
+/* GET Home Page */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Bill Tracker" });
+  Promise.all([Bill.countDocuments({}), Payment.countDocuments({})])
+    .then((counts) => {
+      const [billCount, paymentCount] = counts;
+      res.render("index", {
+        title: "Bill Tracker",
+        data: { bill_count: billCount, payment_count: paymentCount },
+      });
+    })
+    .catch((err) => {
+      res.render("index", { title: "Bill Tracker", error: err });
+    });
 });
 
 // router.get("/bills", bill_controller.index);
